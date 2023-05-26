241fdf4b89496f7c444c05fc55f28509dcdc1709
Fire-The-Fox <gajdos.jan77@gmail.com>
2023-05-26 08:31:21 +0200

    buffer printing functions + line count

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 
 #include "./buffer.c"
 
-int main(int argc, char **argv) {
-    Buffer b = loadFile(argv[1]);
+void printBuffer(Buffer b) {
     for (int i = 0; i < b.lineCount; i++) {
         int end = b.lines[i];
         if (end) {
@@ -14,5 +13,26 @@ int main(int argc, char **argv) {
             free(str);
         }
     }
+}
+
+void printBufferWithLines(Buffer b) {
+    char buff[12];
+    sprintf(buff, "%d", b.lineCount);
+    int width = strlen(buff);
+
+    for (int i = 0; i < b.lineCount; i++) {
+        int end = b.lines[i];
+        if (end) {
+            int start = b.lines[i - 1];
+            char *str = strndup(b.buff + start, end - start - 1);
+            printf("%*d |%s\n", width, i, str);
+            free(str);
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    Buffer b = loadFile(argv[1]);
+    printBufferWithLines(b);
     return 0;
 }
